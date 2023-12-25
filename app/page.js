607cae4bee8bea4d1397bf4eaac1d687c62488fb
sana <ratnavathsana@gmail.com>
2023-12-25 20:28:15 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,79 @@
+"use client"
+import React, { useState } from 'react';
+
+const Page = () => {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [mainTask, setMainTask] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMainTask([...mainTask, { title, desc }]);
+    setDesc('');
+    setTitle('');
+    console.log(mainTask);
+  };
+
+  const deleteHandler = (i) => {
+    let copytask = [...mainTask];
+    copytask.splice(i, 1);
+    setMainTask(copytask);
+  };
+
+  let renderTask = <h2>No Task Available</h2>;
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li key={i} className='flex items-center justify-between'>
+          <div className='flex items-center justify-between mb-8 w-2/3'>
+            <h5 className='text-2xl font-semibold'>{t.title}</h5>
+            <h6 className='text-lg font-medium'>{t.desc}</h6>
+          </div>
+          <button
+            onClick={() => {
+              deleteHandler(i);
+            }}
+            className='bg-red-400 text-white px-4 py-2 rounded font-bold'
+          >
+            Delete
+          </button>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <>
+      <div className='bg-gradient-to-r from-purple-900 to-blue-400 min-h-screen'>
+        <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>MY TODO LIST</h1>
+        <form onSubmit={submitHandler}>
+          <input
+            type='text'
+            className='text-2xl border-zinc-800 border-5 m-4 px-4 py-2'
+            placeholder='Enter Task Here'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <input
+            type='text'
+            className='text-2xl border-zinc-800 border-5 m-4 px-4 py-2'
+            placeholder='Enter Description Here'
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Task</button>
+        </form>
+        <hr />
+        <div className='p-8 bg-slate-200'>
+          <ul>{renderTask}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
+
+
+
